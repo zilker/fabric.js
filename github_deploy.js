5eb277e8ef8ca373753b40a0edb0b415fac1b77d
Valerii Zaochnyi <valery.zaochnuy@desta.software>
2020-02-27 10:58:26 +0200

    +

--- github_deploy.js
+++ github_deploy.js
@@ -1,5 +1,34 @@
-const git = require('simple-git');
+let fs = require('fs'),
+  simpleGit = require('simple-git/promise'),
+  git = simpleGit();
 
-git().status((err, result) => {
-  console.log(result);
+git.status().then(async (status) => {
+  try {
+    if (!status.files.length) {
+      let gitignore = fs.readFileSync('./.gitignore', 'utf8'),
+        regex = new RegExp('/dist', 'i'),
+        result = gitignore.replace(regex, '');
+      console.log(result);
+
+      fs.writeFileSync('./.gitignore', result, 'utf8', function (err) {
+        if (err) return console.log(err);
+      });
+      await git.add('.');
+      await git.commit('Edit .gitignore to publish');
+      await git.raw([
+        'push',
+        'origin',
+        '`git subtree split --prefix dist master`:gh-pages',
+        '--force'
+      ]);
+      await git.reset(['HEAD~']);
+      await git.checkout('.gitignore');
+    } else {
+      console.log('Need clean working directory to publish');
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}).catch(e => {
+  console.error(e);
 });
